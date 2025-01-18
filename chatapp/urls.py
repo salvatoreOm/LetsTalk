1088783b5f9bf44127ba217eae96095d9df501bc
OM PARIHAR <omparihar0001@gmail.com>
2025-01-18 09:13:20 +0530

    link chatroom

--- chatapp/urls.py
+++ chatapp/urls.py
@@ -2,5 +2,7 @@ from django.contrib import admin
 from django.urls import path,include
 from . import views
 urlpatterns = [
-    path('',views.index,name='index'),   
+    path('',views.index,name='index'),  
+    path('<slug:slug>/',views.chatroom,name='chatroom'),
+    
 ]
--- chatapp/views.py
+++ chatapp/views.py
@@ -4,3 +4,9 @@ from .models import ChatRoom
 def index(request):
     chatrooms = ChatRoom.objects.all()
     return render(request,'chatapp/index.html',{'chatrooms':chatrooms})
+
+def chatroom(request,slug):
+    chatroom = ChatRoom.objects.get(slug=slug)
+    return render(request,'chatapp/room.html',{'chatroom':chatroom})
+
+    
