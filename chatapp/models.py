363b516ebab192936914c509bbd13705debc9936
OM PARIHAR <omparihar0001@gmail.com>
2025-01-18 15:14:08 +0530

    set up asgi and asyan and await func

--- chatapp/models.py
+++ chatapp/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class ChatRoom(models.Model):
-    
+   
     def __str__(self):
         return self.name
 
--- chatapp/urls.py
+++ chatapp/urls.py
@@ -3,6 +3,5 @@ from django.urls import path,include
 from . import views
 urlpatterns = [
     path('',views.index,name='index'),  
-    path('<slug:slug>/',views.chatroom,name='chatroom'),
-    
+    path('<slug:slug>/',views.chatroom,name='chatroom')
 ]
