0816fd4a5494bc410fb0c552b45692d11c417bca
OM PARIHAR <omparihar0001@gmail.com>
2025-01-29 18:01:42 +0530

    save message and new model

--- chatapp/consumers.py
+++ chatapp/consumers.py
@@ -1,6 +1,8 @@
 from channels.generic.websocket import AsyncJsonWebsocketConsumer
 import json
 from asgiref.sync import sync_to_async
+from django.contrib.auth.models import User
+from .models import ChatRoom,ChatMessage
 
 class ChatConsumer(AsyncJsonWebsocketConsumer):
    async def connect(self):
@@ -20,3 +22,41 @@ class ChatConsumer(AsyncJsonWebsocketConsumer):
          self.channel_layer,
          self.room_group_name
       )
+
+   async def receive(self,text_data):
+      data = json.loads(text_data)
+      message = data['message']
+      username = data['username']
+      room = data['room']
+
+      await self.channel_layer.group_send(
+         self.room_group_name,{
+            'type':'chat_message',
+            'message':message,
+            'username':username,
+            'room':room,
+         }
+      )
+
+      await self.save_message(username,room,message)
+
+
+
+   async def chat_message(self,event):
+      message = event['message']
+      username = event['username']
+      room = event['room']
+
+      await self.send(text_data=json.dumps({
+         'message':message,
+         'username':username,
+         'room':room,
+      }))
+
+   @sync_to_async
+   def save_message(self,username,room,message):
+      user = User.objects.get(username = username)
+      room = ChatRoom.objects.get(slug=room)
+      ChatMessage.objects.create(user=user, room=room, message_content=message)
+
+   
