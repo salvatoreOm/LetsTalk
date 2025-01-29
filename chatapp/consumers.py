40d92e76ed046a28fdc481d13052aec575802735
OM PARIHAR <omparihar0001@gmail.com>
2025-01-29 16:51:11 +0530

    consumers.py modify

--- chatapp/consumers.py
+++ chatapp/consumers.py
@@ -4,7 +4,7 @@ from asgiref.sync import sync_to_async
 
 class ChatConsumer(AsyncJsonWebsocketConsumer):
    async def connect(self):
-      self.room_name = self.scope['urk_route']['kwargs']['room_name']
+      self.room_name = self.scope['url_route']['kwargs']['room_name']
       self.room_group_name = 'chat_%s' % self.room_name
 
       await self.channel_layer.group_add(
--- chatapp/routing.py
+++ chatapp/routing.py
@@ -3,6 +3,5 @@ from . import consumers
 
 
 websocket_urlpatterns = [
-    path('ws/<str:room_name>', consumers.ChatConsumer.as_asgi()),
-
+    path('ws/<str:room_name>/', consumers.ChatConsumer.as_asgi()),
 ]
