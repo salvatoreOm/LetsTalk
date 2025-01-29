c3edf7700f7af7431ebc45147d246ad5f01ac1d5
OM PARIHAR <omparihar0001@gmail.com>
2025-01-29 23:07:44 +0530

    vercel json file add

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-ekq4=@sg$u(b1$idb8bo-4ywdqh6=fz+i@fm*$!xkdqj2c2j2w
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
